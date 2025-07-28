d7e1d040596a908a157304da74ac2963ca49581e
amirul5656 <hendra.amirul@gmail.com>
2025-07-28 08:47:40 +0700

    Update ytuii.sh

--- ytui.sh
+++ ytui.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Nama acak untuk screen (agar worker unik)
+# Nama acak untuk screen
 RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 
-# Update dan pasang dependensi
+# Update & install dependensi
 apt update -y && apt install -y git screen
 
-# Pindah ke root
+# Masuk ke /root
 cd /root || exit 1
 
-# Clone repo kamu kalau belum ada
+# Clone repo kalau belum ada
 if [ ! -d "new" ]; then
   git clone https://github.com/amirul5656/new.git
 fi
 
-cd new || exit 1
+# Masuk ke folder repo
+cd /root/new || exit 1
 
 # Izin eksekusi
 chmod +x unmi
 
-# Jalankan mining di dalam screen
-screen -dmS "$RAND_NAME" ./unmi --algorithm m7m --pool stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u 9P124xJj1Sq9HgPemLcxLdBxSKn2WYUQ5Q -p c=XMG,zap=XMG
+# Jalankan mining di screen
+screen -dmS "$RAND_NAME" bash -c "cd /root/new && ./unmi --algorithm randomx --pool rx.unmineable.com:3333 --wallet YOURWALLET.$RAND_NAME --coin XMR"
 
-# Info tampilan
+# Info
 echo "🚀 Mining dimulai di screen: $RAND_NAME"
-echo "👀 Untuk lihat: screen -r $RAND_NAME"
+echo "📟 Cek pakai: screen -r $RAND_NAME"
